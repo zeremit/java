4a1aa3c39c2e0c9d2b21bb6871f9945fd5326258
kharevich_d <kharevich_d@Kharevich_D.iba>
2013-03-01 15:04:16 +0300

    Add Product class

--- ExcelParser/src/com/kharevich/logic/Product.java
+++ ExcelParser/src/com/kharevich/logic/Product.java
@@ -1,6 +1,7 @@
 package com.kharevich.logic;
 
 import java.math.BigDecimal;
+import java.sql.Date;
 
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
@@ -50,35 +51,64 @@ public class Product {
 
 	@Column(name = "stock_status_id", nullable = false)
 	private int stock_status_id = 0;
-	
+
 	@Column(name = "image", nullable = true, length = 255)
 	private String image;
-	
+
 	@Column(name = "manufacturer_id", nullable = false)
 	private int manufacturer_id = 0;
-	
+
 	@Column(name = "shipping", columnDefinition = "TINYINT")
 	private int shipping;
-	
-	@Column(name="price", nullable=false)
+
+	@Column(name = "price", nullable = false)
 	private BigDecimal price = BigDecimal.ZERO;
 
 	@Column(name = "points", nullable = true, columnDefinition = "int(8) default 0")
 	private int points;
-	19 	tax_class_id 	int(11) 			Нет 	Нет 		Изменить Изменить 	Удалить Удалить 	Показать больше операций Ещё
-	20 	date_available 	date 			Нет 	Нет 		Изменить Изменить 	Удалить Удалить 	Показать больше операций Ещё
-	21 	weight 	decimal(15,8) 			Нет 	0.00000000 		Изменить Изменить 	Удалить Удалить 	Показать больше операций Ещё
-	22 	weight_class_id 	int(11) 			Нет 	0 		Изменить Изменить 	Удалить Удалить 	Показать больше операций Ещё
-	23 	length 	decimal(15,8) 			Нет 	0.00000000 		Изменить Изменить 	Удалить Удалить 	Показать больше операций Ещё
-	24 	width 	decimal(15,8) 			Нет 	0.00000000 		Изменить Изменить 	Удалить Удалить 	Показать больше операций Ещё
-	25 	height 	decimal(15,8) 			Нет 	0.00000000 		Изменить Изменить 	Удалить Удалить 	Показать больше операций Ещё
-	26 	length_class_id 	int(11) 			Нет 	0 		Изменить Изменить 	Удалить Удалить 	Показать больше операций Ещё
-	27 	subtract 	tinyint(1) 			Нет 	1 		Изменить Изменить 	Удалить Удалить 	Показать больше операций Ещё
-	28 	minimum 	int(11) 			Нет 	1 		Изменить Изменить 	Удалить Удалить 	Показать больше операций Ещё
-	29 	sort_order 	int(11) 			Нет 	0 		Изменить Изменить 	Удалить Удалить 	Показать больше операций Ещё
-	30 	status 	tinyint(1) 			Нет 	0 		Изменить Изменить 	Удалить Удалить 	Показать больше операций Ещё
-	31 	date_added 	datetime 			Нет 	0000-00-00 00:00:00 		Изменить Изменить 	Удалить Удалить 	Показать больше операций Ещё
-	32 	date_modified 	datetime 			Нет 	0000-00-00 00:00:00 		Изменить Изменить 	Удалить Удалить 	Показать больше операций Ещё
-	33 	viewed 	int(5)
 
+	@Column(name = "tax_class_id", nullable = false)
+	private int tax_class_id = 0;
+
+	@Column(name = "date_avalable", nullable = false)
+	private Date date_avalable;
+
+	@Column(name = "weight", nullable = false)
+	private BigDecimal weight = BigDecimal.ZERO;
+
+	@Column(name = "weight_class_id", nullable = false)
+	private int weight_class_id;
+
+	@Column(name = "length", nullable = false)
+	private BigDecimal length = BigDecimal.ZERO;
+
+	@Column(name = "width", nullable = false)
+	private BigDecimal width = BigDecimal.ZERO;
+
+	@Column(name = "height", nullable = false)
+	private BigDecimal height = BigDecimal.ZERO;
+
+	@Column(name = "length_class_id", nullable = false)
+	private int length_class_id;
+
+	@Column(name = "subtract", columnDefinition = "TINYINT")
+	private int subtract = 1;
+
+	@Column(name = "minimum", nullable = false)
+	private int minimum = 1;
+
+	@Column(name = "sort_order", nullable = false)
+	private int sort_order = 0;
+
+	@Column(name = "status", columnDefinition = "TINYINT")
+	private int status = 0;
+
+	@Column(name = "date_added", nullable = false)
+	private Date date_added;
+
+	@Column(name = "date_modified", nullable = false)
+	private Date date_modified;
+
+	@Column(name = "viewed", nullable = false)
+	private int viewed = 0;
 }
