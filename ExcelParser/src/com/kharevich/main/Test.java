5c2172b7fd23638a0f5a060350cf26fb8cf2c8c5
zeremit <zeremit@zeremit-laptop>
2013-03-11 01:08:07 +0300

    Test class

---
+++ ExcelParser/src/com/kharevich/main/Test.java
@@ -0,0 +1,141 @@
+package com.kharevich.main;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.kharevich.logic.Product;
+import com.kharevich.logic.ProductDescription;
+import com.kharevich.logic.ProductToCategory;
+import com.kharevich.logic.ProductToStore;
+import com.kharevich.util.ExcelHelper;
+import com.kharevich.util.HibernateUtil;
+
+public class Test {
+
+	private static ClassPathXmlApplicationContext ac;
+
+	private static final BigDecimal devide = new BigDecimal("8600");
+
+	public static void main(String[] args) throws Exception {
+		Session session = null;
+		Transaction tx = null;
+		// ProductDescription user = null;
+		// try {
+		// session = HibernateUtil.getSessionFactory().openSession();
+		// tx = session.beginTransaction();
+		// user = (ProductDescription) session.get(ProductDescription.class,
+		// (long) 1583);
+		// tx.commit();
+		//
+		// } catch (Exception e) {
+		// if (tx != null)
+		// tx.rollback();
+		// throw e;
+		// } finally {
+		// session.close();
+		// }
+		// System.out.println(user.toParams());
+
+		ac = new ClassPathXmlApplicationContext(new String[] { "config.xml" });
+		FileInputStream file = new FileInputStream(new File("base24.xls"));
+		HSSFWorkbook workbook = new HSSFWorkbook(file);
+
+		HSSFSheet sheet = workbook.getSheetAt(0);
+		Iterator<Row> rowIterator = sheet.iterator();
+		rowIterator.next();
+		Set<Integer> type = new HashSet<Integer>();
+		while (rowIterator.hasNext()) {
+			ProductDescription productdescription = (ProductDescription) ac
+					.getBean("product_description_base");
+			ProductToStore productToStore = (ProductToStore) ac
+					.getBean("product_to_store_base");
+			ProductToCategory productToCategory = (ProductToCategory) ac
+					.getBean("product_to_category_base");
+			Row row = rowIterator.next();
+			// Product product = new Product();
+			type.add(row.getCell(1).getCellType());
+			ExcelHelper.getStatus(row.getCell(8));
+//			System.out.println(ExcelHelper.getStatus(row.getCell(8)));
+			// product.setCode((int) row.getCell(1).getNumericCellValue());
+			// product.setOkpd(row.getCell(11).getStringCellValue());
+			// product.setName(row.getCell(2).getStringCellValue());
+			// product.setDescription(row.getCell(4).getStringCellValue());
+			// System.out.println(row.getCell(3));
+			//
+			//
+			//
+			//
+			// System.out.println(row.getCell(5));
+//			try {
+//				session = HibernateUtil.getSessionFactory().openSession();
+//				tx = session.beginTransaction();
+//				@SuppressWarnings("unchecked")
+//				List<Product> result = session
+//						.createCriteria(Product.class)
+//						.add(Restrictions.eq("partner_product_id", (long) row
+//								.getCell(0).getNumericCellValue()))
+//						.setMaxResults(1).list();
+//				Product product = null;
+//				if (result.size() > 0) {
+//					product = result.get(0);
+//					session.update(product);
+//				} else {
+//					product = (Product) ac.getBean("product_base");
+//					String s = ExcelHelper.getString(row.getCell(2));
+//					product.setModel(s);
+//					product.setPartner_product_id((long) row.getCell(0)
+//							.getNumericCellValue());
+//					product.setCode(ExcelHelper.getString(row.getCell(1)));
+//					product.setOkdp(ExcelHelper.getString(row.getCell(11)));
+//					product.setSku(ExcelHelper.getString(row.getCell(3)));
+//					System.out.println(product.getPartner_product_id());
+//					BigDecimal price = new BigDecimal(ExcelHelper.getString(row
+//							.getCell(5)).replaceAll("[\']","").replaceAll(",",".")).divide(devide, 1,
+//							RoundingMode.HALF_UP);
+//					product.setPrice(price);
+//					price = new BigDecimal(
+//							ExcelHelper.getString(row.getCell(6))).divide(
+//							devide, 1, RoundingMode.HALF_UP);
+//					product.setPartner_price(price);
+//					productdescription.setDescription(ExcelHelper.getString(row
+//							.getCell(4)));
+//					productdescription.setName(s);
+//					session.save(product);
+//					productdescription.setProduct_id(product.getProduct_id());
+//					session.save(productdescription);
+//					productToStore.setProduct_id(product.getProduct_id());
+//					session.save(productToStore);
+//					productToCategory.setProduct_id(product.getProduct_id());
+//					session.save(productToCategory);
+//				}
+//				// try {
+//				// session = HibernateUtil.getSessionFactory().openSession();
+//				// tx = session.beginTransaction();
+//
+//				 tx.commit();
+//			} catch (Exception e) {
+//				if (tx != null)
+//					tx.rollback();
+//				throw e;
+//			} finally {
+//				session.close();
+//			}
+		}
+
+	}
+
+}
--- ExcelParser/src/com/kharevich/util/ExcelHelper.java
+++ ExcelParser/src/com/kharevich/util/ExcelHelper.java
@@ -1,10 +1,22 @@
 package com.kharevich.util;
 
 import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import org.apache.poi.ss.usermodel.Cell;
 
 public class ExcelHelper {
+
+	private static Map<String, Integer> status = new HashMap<String, Integer>();
+	{
+		status.put("резерв", 0);
+		status.put("дней", 100);
+		status.put("дня", 100);
+		status.put("день", 100);
+	};
+
 	public static String getString(Cell cell) {
 		switch (cell.getCellType()) {
 		case Cell.CELL_TYPE_STRING:
@@ -17,5 +29,24 @@ public class ExcelHelper {
 		}
 		return "";
 
+	}
+
+	public static int getStatus(Cell cell) {
+		switch (cell.getCellType()) {
+		case Cell.CELL_TYPE_STRING:
+			String s = cell.getStringCellValue().toLowerCase();
+			for (Entry<String, Integer> pair : status.entrySet()) {
+				if (s.contains(pair.getKey())) {
+					System.out.println("ok");
+					return pair.getValue();
+				}
+			}
+			System.out.println("err"+s);
+			break;
+		case Cell.CELL_TYPE_NUMERIC:
+			cell.getNumericCellValue();
+
+		}
+		return 0;
 	}
 }
