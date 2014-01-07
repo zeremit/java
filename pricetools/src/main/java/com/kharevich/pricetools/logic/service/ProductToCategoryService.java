b3ad3aa6bfead8050da3b2203ca34fdca875ee4c
zeremit <zeremit@zeremit-aspire>
2014-01-07 22:30:59 +0300

    service

---
+++ pricetools/src/main/java/com/kharevich/pricetools/logic/service/ProductToCategoryService.java
@@ -0,0 +1,9 @@
+package com.kharevich.pricetools.logic.service;
+
+import com.kharevich.pricetools.logic.ProductToCategory;
+
+public interface ProductToCategoryService {
+	
+	public void addProduct(ProductToCategory productToCategory);
+
+}
---
+++ pricetools/src/main/java/com/kharevich/pricetools/logic/service/ProductToCategoryServiceImpl.java
@@ -0,0 +1,23 @@
+package com.kharevich.pricetools.logic.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.kharevich.pricetools.logic.ProductToCategory;
+import com.kharevich.pricetools.logic.dao.ProductToCategoryDAO;
+
+@Service("productToCategoryService")
+public class ProductToCategoryServiceImpl implements ProductToCategoryService {
+
+	@Autowired
+	private ProductToCategoryDAO dao;
+	
+	@Override
+	@Transactional
+	public void addProduct(ProductToCategory productToCategory) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
