7e13b0d1314c32f6369601a506898add845c1962
dmitry.khorevich <dmitry.khorevich@altoros.com>
2014-02-10 15:41:31 +0300

    log4j

--- pricetools/src/main/java/com/kharevich/pricetools/App.java
+++ pricetools/src/main/java/com/kharevich/pricetools/App.java
@@ -5,16 +5,22 @@ import java.io.File;
 import java.io.InputStreamReader;
 import java.math.BigDecimal;
 
+import org.apache.log4j.Logger;
+
 import com.kharevich.pricetools.util.DownloadManager;
-import com.kharevich.pricetools.util.FTPUtil;
-import com.kharevich.pricetools.util.HTMLPicParser;
+
 /**
  * Hello world!
  * 
  */
 public class App {
+	public static final Logger LOG = Logger.getLogger(App.class);
+
 	public static void main(String[] args) throws Exception {
-		//System.setOut(new java.io.PrintStream(System.out, true, "Cp866"));
+		// System.setOut(new java.io.PrintStream(System.out, true, "Cp866"));
+//		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(
+//				"applicationContext.xml");
+		LOG.info("Application start");
 		while (true) {
 			BufferedReader br = new BufferedReader(new InputStreamReader(
 					System.in));
@@ -42,7 +48,7 @@ public class App {
 							"http://www.tools.by/base24.php", "base24.xls",
 							true);
 					UploadImage.proceed(image);
-				//	UpdateImageLinkHTML.proceed(image);
+					// UpdateImageLinkHTML.proceed(image);
 					break;
 				case 3:
 					// FTPUtil.deleteFiles();
@@ -53,7 +59,7 @@ public class App {
 			} catch (NumberFormatException nfe) {
 				System.err.println("Неверный формат!");
 			}
-			//FTPUtil.deleteFiles();
+			// FTPUtil.deleteFiles();
 		}
 	}
 }
